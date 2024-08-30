ae859e96f8e976e4ac78bd2d5982ac3596c0d682
Senny Kalidien <sennykalidien@gmail.com>
2024-08-30 23:09:23 +0200

    fix: more gap between overview grid items

--- components/overview/overview-grid.vue
+++ components/overview/overview-grid.vue
@@ -9,7 +9,7 @@ defineProps<Props>();
 </script>
 
 <template>
-  <ul class="flex gap-2 flex-wrap justify-between">
+  <ul class="flex gap-10 flex-wrap justify-between">
     <li v-for="item in items" :key="`overview-grid-item${item.id}`" class="w-[28ch]">
       <NuxtLink :to="item.url" class="h-full">
         <UCard class="h-full">
