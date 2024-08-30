b363f930ebdc34282fa313cd58aabe1b2284392c
Senny Kalidien <sennykalidien@gmail.com>
2024-08-30 23:43:31 +0200

    fix: conditional slot for layout-page

--- components/layout/layout-page.vue
+++ components/layout/layout-page.vue
@@ -3,10 +3,10 @@
     <header v-if="$slots['page-header']" class="bg-primary text-center py-10">
       <slot name="page-header" />
     </header>
-    <div>
+    <div v-if="$slots.default">
       <slot />
     </div>
-    <footer>
+    <footer v-if="$slots['page-footer']">
       <slot name="page-footer" />
     </footer>
   </section>
